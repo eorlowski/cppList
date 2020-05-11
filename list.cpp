4d11119670e242b8708c1a864ae05d4c2e71c3b6
eorlowski <edward.orlowski@ah.nl>
2020-05-11 14:32:37 +0200

    Added find in string

--- list.cpp
+++ list.cpp
@@ -10,6 +10,8 @@ int main(int argc, char** argv) {
 	intList.push_back(1);
 	intList.push_back(2);
 
+	int result;
+
 	for (int elem : intList) {
 		cout << "Found list element: " << elem << endl;
 	}
@@ -19,6 +21,20 @@ int main(int argc, char** argv) {
 	cout << a << endl;
 
 	cout << a.substr(2, 2) << endl;
+
+	cout << "Does the string contain ello?:" << a.find("llo") << endl;
+	// Is cast needed to get a signed int?
+	cout << "Does the string contain ello?:" << (int) a.find("abc") << endl;
+
+	result = a.find("abd");
+	cout << "Result is: " << result << endl;
+
+	if (result == -1) {
+		cout << "Found abd in string" << endl;
+	}
+	else {
+		cout << "Did NOT found abd in string" << endl;
+	}
 
 	return 0;
 }
